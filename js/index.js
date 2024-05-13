9c93c2a30a060da4ffa3923d5f0ddd64c1fce3cf
prajwoldangol <prajwoldangol@gmail.com>
2024-05-13 15:59:52 -0700

    login page

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const darkButton = document.querySelector("#dark-mode");
+a;
+function darkMode() {
+  loginContainer.style.backgroundColor = "#030712";
+  loginContainer.style.color = white;
+}
+
+const light = document.querySelector("#dark-mode");
+const loginContainer = document.querySelector(".login-container");
+// const login = documen
+// ""()tceleSyreuq.
+light.addEventListener("click", lightMode());
+
+function lightMode() {
+  loginContainer.style.backgroundColor = rgba(255, 255, 255, 0.85);
+  loginContainer.style.color = "#030712";
+}
