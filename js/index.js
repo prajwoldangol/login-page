041db758a7b69f4e6007564e8a2858eaa463582b
prajwoldangol <prajwoldangol@gmail.com>
2024-05-13 23:08:40 -0700

    Final Page

--- js/index.js
+++ js/index.js
@@ -1,17 +1,9 @@
-const darkButton = document.querySelector("#dark-mode");
-a;
-function darkMode() {
-  loginContainer.style.backgroundColor = "#030712";
-  loginContainer.style.color = white;
-}
+const darkButton = document.getElementById("dark-mode");
+const light = document.getElementById("light-mode");
 
-const light = document.querySelector("#dark-mode");
-const loginContainer = document.querySelector(".login-container");
-// const login = documen
-// ""()tceleSyreuq.
-light.addEventListener("click", lightMode());
-
-function lightMode() {
-  loginContainer.style.backgroundColor = rgba(255, 255, 255, 0.85);
-  loginContainer.style.color = "#030712";
-}
+darkButton.addEventListener("click", () => {
+  document.body.classList.add("dark-mode");
+});
+light.addEventListener("click", () => {
+  document.body.classList.remove("dark-mode");
+});
